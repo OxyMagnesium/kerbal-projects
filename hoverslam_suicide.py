cbdc53b15ad8cf6bd2f856656d3e2632756b2b47
Om_G <34579088+OmG-117@users.noreply.github.com>
2019-02-22 20:26:57 +0530

    Set up proper data processing from flight object, removed testing code.

--- hoverslam_suicide.py
+++ hoverslam_suicide.py
@@ -1,42 +1,26 @@
 from math import log
 
-g = 9.8
-isp = 300
-mass = 30000
-dry_mass = 20000
-thrust = 750000
-t = 0
+def stoppingDist(vessel, max_g):
+    mass = vessel.mass
+    dry_mass = vessel.dry_mass
+    isp = vessel.specific_impulse
+    thrust = mass*g*max_g if mass*g*max_g < vessel.available_thrust else vessel.available_thrust
+    v = vessel.flight(vessel.orbit.body.reference_frame).vertical_speed
 
-def suicideDeltaV(t):
-    return -g*(isp*log(1 - (thrust*t)/(mass*isp*g)) + t)
-
-def velocityKillTime(v):
     upper_lim = (mass - dry_mass)*(isp*g)/thrust
     lower_lim = 0
     t = 0
     dv = 0
-    max_cycles = 10
+    max_cycles = 20
 
     while abs(v - dv) > 1 and max_cycles > 0:
         t = (upper_lim + lower_lim)/2
-        dv = suicideDeltaV(t)
-        print(dv, t)
+        dv = -g*(isp*log(1 - (thrust*t)/(mass*isp*g)) + t)
         if dv > v:
             upper_lim = t
         else:
             lower_lim = t
         max_cycles -= 1
 
-    return t
-
-def stoppingDist(v):
-    t = velocityKillTime(v)
     return -g*(2*isp*(thrust*t - mass*isp*g)*log(1 - (thrust*t)/(mass*isp*g))
                + thrust*t*(t - 2*isp))/(2*thrust)
-
-speed = input()
-if speed == "":
-    speed = 250
-else:
-    speed = int(speed)
-print(stoppingDist(speed))
